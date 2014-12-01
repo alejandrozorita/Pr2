9b447a52ac2c1743be9ee9ea5a7e9b53711b223d
nroje <nrojec@gmail.com>
2014-12-01 19:06:54 +0100

    Reglas juego terminada y añadida clase moviminento

---
+++ src/tp/pr2/logica/Movimiento.java
@@ -0,0 +1,10 @@
+package tp.pr2.logica;
+
+public class Movimiento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tp/pr2/logica/ReglasJuego.java
@@ -0,0 +1,5 @@
+package tp.pr2.logica;
+
+public abstract class ReglasJuego {
+
+}
